c6cd04823c22ef9af770d3034a264c09788e743a
Jacob Wright <jacobwright@GX00096-MacBookPro.local>
2022-04-05 17:21:31 -0600

    beginning results comp

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header'
 import Nav from '../components/Nav'
+import Results from '../components/Results'
 
 export default function Home() {
   return (
@@ -16,6 +17,7 @@ export default function Home() {
       {/* Navbar */}
       <Nav />
       {/* Results */}
+      <Results />
     </div>
   )
 }
