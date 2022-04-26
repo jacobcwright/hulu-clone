b4c788783b6b9bb10a3bf3b8ea6dea62c69ab2ca
Jacob Wright <36747872+jacobcwright@users.noreply.github.com>
2022-04-26 16:35:40 -0600

    fixed nextjs warnings

--- components/header.js
+++ components/header.js
@@ -24,6 +24,7 @@ function Header() {
       <Image
         className="object-contain"
         src="https://links.papareact.com/ua6"
+        alt="hulu logo"
         width={200}
         height={100}
       />
