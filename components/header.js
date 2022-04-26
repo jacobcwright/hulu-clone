f09a473544d6945b2dd8f6157e8d8cc7f3408ded
Jacob Wright <36747872+jacobcwright@users.noreply.github.com>
2022-04-26 16:33:12 -0600

    fixed header typo

--- components/header.js
+++ components/header.js
@@ -1,27 +1,34 @@
-import Image from 'next/image'
-import React from 'react'
-import HeaderItem from './HeaderItem'
-import { BadgeCheckIcon, CollectionIcon, HomeIcon, LightningBoltIcon, SearchIcon, UserIcon } from '@heroicons/react/outline'
+import Image from "next/image";
+import React from "react";
+import HeaderItem from "./HeaderItem";
+import {
+  BadgeCheckIcon,
+  CollectionIcon,
+  HomeIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UserIcon,
+} from "@heroicons/react/outline";
 
 function Header() {
   return (
-    <header className='flex flex-col sm:flex-row m-5 justify-between items-center'>
-        <div className='flex flex-grow justify-evenly max-w-2xl h-auto'>
-            <HeaderItem title="HOME" Icon={HomeIcon}/>
-            <HeaderItem title="TRENDING" Icon={LightningBoltIcon}/>
-            <HeaderItem title="VERIFIED" Icon={BadgeCheckIcon}/>
-            <HeaderItem title="COLLECTIONS" Icon={CollectionIcon}/>
-            <HeaderItem title="SEARCH" Icon={SearchIcon}/>
-            <HeaderItem title="ACCOUNT" Icon={UserIcon}/>
-
-        </div>
-        <Image 
+    <header className="flex flex-col sm:flex-row m-5 justify-between items-center">
+      <div className="flex flex-grow justify-evenly max-w-2xl h-auto">
+        <HeaderItem title="HOME" Icon={HomeIcon} />
+        <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
+        <HeaderItem title="VERIFIED" Icon={BadgeCheckIcon} />
+        <HeaderItem title="COLLECTIONS" Icon={CollectionIcon} />
+        <HeaderItem title="SEARCH" Icon={SearchIcon} />
+        <HeaderItem title="ACCOUNT" Icon={UserIcon} />
+      </div>
+      <Image
         className="object-contain"
-        src="https://links.papareact.com/ua6" 
+        src="https://links.papareact.com/ua6"
         width={200}
-        height={100}/>
-        </header>
-  )
+        height={100}
+      />
+    </header>
+  );
 }
 
-export default Header
+export default Header;
